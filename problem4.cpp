4b4fae53e4c2fc22575ccb73bae4ac7d13ae1091
Alisher <113435407+220108cs10@users.noreply.github.com>
2022-10-12 13:16:25 +0500

    Add files via upload

---
+++ problem4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv []) {
+    int n;
+    cout<<"Month number :";
+    cin>>n;
+    switch (n){
+        case 1:cout<<"January - 31 days\n";
+        break;
+        case 2:cout<<"February - 28 days in a common year and 29 days in ,leap yeras\n";
+        break;
+        case 3:cout<<"March - 31 days\n";
+        break;
+        case 4:cout<<"April - 30 days\n";
+        break;
+        case 5:cout<<"May - 31 days\n";
+        break;
+        case 6:cout<<"June - 30 days\n";
+        break;
+        case 7:cout<<"July - 31 days\n";
+        break;
+        case 8:cout<<"August - 31 days\n";
+        break;
+        case 9:cout<<"September - 30 days\n";
+        break;
+        case 10:cout<<"October - 31 days\n";
+        break;
+        case 11:cout<<"November - 30 days\n";
+        break;
+        case 12:cout<<"December - 31 days\n";
+        break;
+        default: cout<<"Error\n";
+        break;
+    }
+
+
+    return 0;
+}
