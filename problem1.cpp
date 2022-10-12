62861b746b0514051a713a30e73490fcfd2edff6
Alisher <113435407+220108cs10@users.noreply.github.com>
2022-10-12 13:09:21 +0500

    Add files via upload

---
+++ problem1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int age;
+    cout<<"enter youre age :";
+    cin>>age;
+    if ( age >= 18){
+        cout<<"you can ride a car";
+    }else{
+        cout <<"you can't ride a car ";
+    }
+
+    return 0;
+}
